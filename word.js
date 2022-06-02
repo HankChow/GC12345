cc8bcb138f5db62fbedfc7c15546750499d9ec39
Hank Chow <280630620@qq.com>
2022-06-02 15:54:54 +0800

    Create word.js

---
+++ word.js
@@ -0,0 +1,35 @@
+function element_count(arr) {
+	let count = {};
+	for(let i = 0; i < arr.length; i++) {
+		if (count[arr[i]] != undefined) {count[arr[i]]++;}
+		else {count[arr[i]] = 1;}
+	}
+	return count;
+}
+
+function wordle(word) {
+	answer = "sheet";
+	answer = answer.split("");
+	answer_elements = element_count(answer);
+	guess = word.split("");
+	result = ["C", "C", "C", "C", "C"];
+	// mark A first
+	for(i = 0; i < 5; i++) {
+		if(guess[i] == answer[i]) {
+			result[i] = "A"
+			answer_elements[guess[i]]--;
+		}
+	}
+	// then mark B
+	for(i = 0; i < 5; i++) {
+		if(letters[i] != answer[i]) {
+			if(answer.includes(guess[i]) && answer_elements[guess[i]] > 0) {
+				result[i] = "B"
+				answer_elements[guess[i]]--;
+			}
+		} else {
+			// others remain C
+		}
+	}
+	console.log(result)
+}
